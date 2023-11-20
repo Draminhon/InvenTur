eb44a8b1084fcff5efa4917e4f79872f8db2bd8f
Tiago Alves de Lima <tiago.alves07@aluno.ifce.edu.br>
2023-11-19 22:03:27 -0300

    login page form

--- src/inventur/lib/pages/login_page.dart
+++ src/inventur/lib/pages/login_page.dart
@@ -8,12 +8,28 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController cpfController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          
+          children: [
+            TextFormField(
+              autofocus: true,
+              controller: cpfController,
+              decoration: const InputDecoration(
+                
+              ),
+            ),
+            TextFormField(),
+            ElevatedButton(onPressed: () {}, child: const Text('ENTRAR')),
+            TextButton(onPressed: () {}, child: const Text('Esqueceu sua senha?')),
+            TextButton(onPressed: () {}, child: const Text('Registre-se')),
+          ],
         ),
       ),
     );
