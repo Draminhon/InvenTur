e23b14621a61b7bef45d5e9bfbd4d83cb6ac9ef7
Tiago Alves de Lima <tiago.alves07@aluno.ifce.edu.br>
2023-11-28 16:31:59 -0300

    tests do codigo de validação do email

--- src/inventur/test/validators/email_verification_code_validator_test.dart
+++ src/inventur/test/validators/email_verification_code_validator_test.dart
@@ -1,8 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 
 class EmailVerificationCodeValidator {
-  validate({int? code}) {
-    if (code == null) return 'O código deve ser informado';
+  validate({String? code}) {
+    if (code == null || code == '') return 'O código deve ser informado';
+
+    int intCode;
+    try {
+      intCode = int.parse(code);
+    } catch (e) {
+      return 'O código é formado apenas por números';
+    }
   }
 }
 
@@ -22,6 +29,22 @@ void main() {
           final result = emailVerificationCodeValidator.validate();
 
           expect(result, 'O código deve ser informado');
+        }
+      );
+      test(
+        'deve retornar uma mensagem de erro caso o código seja vazio', 
+        () {
+          final result = emailVerificationCodeValidator.validate();
+
+          expect(result, 'O código deve ser informado');
+        }
+      );
+      test(
+        'deve retornar uma mensagem de erro caso o código não seja numérico', 
+        () {
+          final result = emailVerificationCodeValidator.validate(code: 'asfsd');
+
+          expect(result, 'O código é formado apenas por números');
         }
       );
     },
