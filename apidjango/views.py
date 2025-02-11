ad6f368b82a0e6c30a6c0c8978f356305c3c68f2
Murilo <britomurilorog@gmail.com>
2025-02-11 18:52:02 -0300

    ajeitando view

--- apidjango/views.py
+++ apidjango/views.py
@@ -76,7 +76,7 @@ class PesquisaCreateView(generics.ListCreateAPIView):
 class PesquisaUsuarioListView(generics.ListAPIView):
     serializer_class = PesquisaSerializer
     permission_classes = [permissions.IsAuthenticated] 
-    authentication_classes = [JWTAuthentication]
+   # authentication_classes = [JWTAuthentication]
     def get_queryset(self):
         print(self.request.headers)
         user = self.request.user
